520cfc315a9dd1c1f8b2c0a217b7bdeba5600252
Patricia Nykaza <pnykaza@calstatela.edu>
2018-06-25 18:05:13 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -25,7 +25,7 @@ class Song
   end
   
   def self.find_by_name(name)
-    @@all.detect{|song|song.name}
+    @@all.detect{ |song| song.name == name}
   end
   
   def self.find_or_create_by_name(name)
